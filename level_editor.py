a8eb9189e10b82ecf77e2ddc1a73d88faa0f75ad
yanyan <yanyan032015@icloud.com>
2026-04-16 13:59:26 +0900

    初期込みっと

---
+++ level_editor.py
@@ -0,0 +1,24 @@
+import bpy
+
+bl_info = {
+    "name": "レベルエディタ",
+    "author": "Taro Kamata",
+    "version": (1, 0),
+    "blender": (3, 3, 1),
+    "location": "",
+    "description": "レベルエディタ",
+    "warning": "",
+    "support": "TESTING",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Object"
+}
+
+def register():
+    print("レベルエディタが有効化されました。")
+
+def unregister():
+    print("レベルエディタが無効化されました。")
+
+if __name__ == "__main__":
+    register()
